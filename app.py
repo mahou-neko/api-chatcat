47ac0b92574a91d3d1bf403189d076fadbd4e689
Stephanie Autherith <stephanie.autherith@student.uibk.ac.at>
2017-07-25 19:14:01 +0200

    Added some layer data for asking about specific layers fullfilment

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ def makeWebhookResultTriggerEvent():
         # "data": data,
         # "contextOut": [],
         "source": "apiai-weather-webhook-sample"
-        "followupEvent":{"name":"eventtry"}
+        "followupEvent":"eventtry"
     }
 
 def makeWebhookResultLayerAbout(layer):
